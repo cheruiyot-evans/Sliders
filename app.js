5ca1773613e356a36188eee1084e8193eb972961
Cheruiyot Evans <chersoftwaredev@gmail.com>
2026-02-06 18:09:49 +0300

    prev review button

--- app.js
+++ app.js
@@ -44,9 +44,24 @@ const startSlider = (type) => {
   const active = getElement('.active');
   const last = getElement('.last');
   let next = active.nextElementSibling;
+  if (!next) {
+    next = container.firstElementChild;
+  }
   active.classList.remove(['active']);
   last.classList.remove(['last']);
   next.classList.remove(['next']);
+
+  if (type === 'prev') {
+    active.classList.add('next');
+    last.classList.add('active');
+    next = last.previousElementSibling;
+    if (!next) {
+      next = container.lastElementChild;
+    }
+    next.classList.remove(['next']);
+    next.classList.add('last');
+    return;
+  }
 
   active.classList.add('last');
   last.classList.add('next');
@@ -54,8 +69,8 @@ const startSlider = (type) => {
 };
 
 nextBtn.addEventListener('click', () => {
-  startSlider('prev');
+  startSlider();
 });
 prevBtn.addEventListener('click', () => {
-  startSlider();
+  startSlider('prev');
 });
