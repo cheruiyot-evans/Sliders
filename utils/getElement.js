9e7f6016483eb1dd15c7b4a0f8dae9b864b2689e
Cheruiyot Evans <chersoftwaredev@gmail.com>
2026-02-06 06:16:54 +0300

    review setup

--- utils/getElement.js
+++
@@ -1,9 +0,0 @@
-const getElement = (selection) => {
-  const element = document.querySelector(selection);
-  if (element) {
-    return element;
-  }
-  throw new Error('No item selected try again with the correct selection');
-};
-
-export default getElement;
