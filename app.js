6d67ba225d0f2494092ef4da128e441b9b433532
Cheruiyot Evans <chersoftwaredev@gmail.com>
2026-02-06 17:58:54 +0300

    next review button

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 import people from './data.js';
 import getElement from './utils/getElement.js';
 
-const container = getElement('.slider-container');
+const container = getElement('.slide-container');
 const nextBtn = getElement('.next-btn');
 const prevBtn = getElement('.prev-btn');
 
 const displayReview = () => {
-  const slider = getElement('.slider');
   const peopleDisplay = people
     .map((person, slideIndex) => {
-      console.table(person);
+      // console.table(person);
       const { img, name, job, text } = person;
+      let position = 'next';
+      if (slideIndex === 0) {
+        position = 'active';
+      }
+      if (slideIndex === people.length - 1) {
+        position = 'last';
+      }
       return `
-   <article class="slide">
+   <article class="slide ${position}">
           <img
             src="${img}"
             class="img"
             alt="${name}"
           />
-          <h4>peter</h4>
+          <h4>${name}</h4>
           <p class="title">${job}</p>
           <p class="text">${text}
           </p>
@@ -33,3 +39,23 @@ const displayReview = () => {
 };
 
 displayReview();
+
+const startSlider = (type) => {
+  const active = getElement('.active');
+  const last = getElement('.last');
+  let next = active.nextElementSibling;
+  active.classList.remove(['active']);
+  last.classList.remove(['last']);
+  next.classList.remove(['next']);
+
+  active.classList.add('last');
+  last.classList.add('next');
+  next.classList.add('active');
+};
+
+nextBtn.addEventListener('click', () => {
+  startSlider('prev');
+});
+prevBtn.addEventListener('click', () => {
+  startSlider();
+});
